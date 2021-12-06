7c5b6c683a5ef66e1323f1d3901bf29acc558b87
erzhan <erzhan.sarsenov@gmail.com>
2021-12-06 20:58:40 +0600

    feat: Even game added

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/maintainability"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability" /></a>
 
-![example workflow](https://github.com/erzhan12/backend-project-lvl1/actions/workflows/lint.yaml/badge.svg)
+![Test Coverage](https://github.com/erzhan12/backend-project-lvl1/actions/workflows/lint.yaml/badge.svg)
--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,35 @@ const greeting = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
+  return name;
 };
 
-export default greeting;
+const question = () => {
+  const num = Math.round(Math.random() * 100);
+  console.log(`Question ${num}`);
+  const answer = readlineSync.question('Your answer: ');
+  if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer !== 'yes')) {
+    return true;
+  }
+  return false;
+};
+
+const even = (name) => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let rightAnswerRow = 0;
+  while (rightAnswerRow < 3) {
+    if (question()) {
+      rightAnswerRow += 1;
+    } else {
+      rightAnswerRow = 0;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+const brainEven = () => {
+  const name = greeting();
+  even(name);
+};
+
+export { greeting, brainEven };
