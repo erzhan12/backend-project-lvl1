4a9770c23b984c35e984dcb51cccfb5aee7d3fa4
erzhan <erzhan.sarsenov@gmail.com>
2021-12-08 14:10:08 +0600

    feat: Brain Calc game added

--- src/cli.js
+++ src/cli.js
@@ -7,32 +7,62 @@ const greeting = () => {
   return name;
 };
 
-const question = () => {
+const questionEven = () => {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
   const num = Math.round(Math.random() * 100);
   console.log(`Question ${num}`);
-  const answer = readlineSync.question('Your answer: ');
-  if ((num % 2 === 0 && answer === 'yes') || (num % 2 !== 0 && answer !== 'yes')) {
-    return true;
+  const userAnswer = readlineSync.question('Your answer: ');
+  let correctAnswer = '';
+  if (num % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
   }
-  return false;
+  return { userAnswer, correctAnswer };
 };
 
-const even = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const questionary = (name, questionFunc) => {
   let rightAnswerRow = 0;
+  let questionResult = null;
   while (rightAnswerRow < 3) {
-    if (question()) {
+    questionResult = questionFunc();
+    if (questionResult.userAnswer === questionResult.correctAnswer) {
       rightAnswerRow += 1;
+      console.log('Correct!');
     } else {
       rightAnswerRow = 0;
+      console.log(`'${questionResult.userAnswer}' is wrong answer ;(. Correct answer was '${questionResult.correctAnswer}'.`);
+      return;
     }
   }
   console.log(`Congratulations, ${name}!`);
 };
 
-const brainEven = () => {
-  const name = greeting();
-  even(name);
+const generateExpression = () => {
+  const n1 = Math.round(Math.random() * 100);
+  const n2 = Math.round(Math.random() * 100);
+  const operandIndex = Math.floor(Math.random() * 3);
+  const operands = ['+', '-', '*'];
+  return `${n1} ${operands[operandIndex]} ${n2}`;
 };
 
-export { greeting, brainEven };
+const questionCalc = () => {
+  console.log('What is the result of the expression?');
+  const expression = generateExpression();
+  console.log(`Question ${expression}`);
+  const correctAnswer = eval(expression).toString();
+  const userAnswer = readlineSync.question('Your answer: ');
+  return { userAnswer, correctAnswer };
+};
+// ----------------------------------------
+const brainEven = () => {
+  const name = greeting();
+  questionary(name, questionEven);
+};
+
+const brainCalc = () => {
+  const name = greeting();
+  questionary(name, questionCalc);
+};
+
+export { greeting, brainEven, brainCalc };
