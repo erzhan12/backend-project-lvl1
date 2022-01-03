ec6cb2939cbb206097414bde1c04f8249871d72a
erzhan <erzhan.sarsenov@gmail.com>
2022-01-04 02:21:43 +0600

    fix: no-eval

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -3,18 +3,26 @@
 import readlineSync from 'readline-sync';
 import { greeting, questionary } from '../src/cli.js';
 
+const evalExpression = (n1, n2, op) => {
+  if (op === '+') return n1 + n2;
+  if (op === '-') return n1 - n2;
+  if (op === '*') return n1 * n2;
+  return '';
+};
+
 const generateExpression = () => {
   const n1 = Math.round(Math.random() * 100);
   const n2 = Math.round(Math.random() * 100);
   const operandIndex = Math.floor(Math.random() * 3);
   const operands = ['+', '-', '*'];
-  return `${n1} ${operands[operandIndex]} ${n2}`;
+  const correctAnswer = evalExpression(n1, n2, operands[operandIndex]).toString();
+  const expression = `${n1} ${operands[operandIndex]} ${n2}`;
+  return { expression, correctAnswer };
 };
 
 const questionCalc = () => {
-  const expression = generateExpression();
+  const { expression, correctAnswer } = generateExpression();
   console.log(`Question: ${expression}`);
-  const correctAnswer = eval(expression).toString();
   const userAnswer = readlineSync.question('Your answer: ');
   return { userAnswer, correctAnswer };
 };
